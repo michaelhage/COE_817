e88e498ac66f22fe37c13683825d87bd92d131f3
Michael Hage <mhage222@gmail.com>
2020-01-20 17:02:40 -0500

    changes to project 2

---
+++ Labs/lab1/Project2/Project2/src/pro1/Vigenere.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pro1;
+
+/**
+ *
+ * @author Michael Hage
+ */
+public class Vigenere {
+    public String encode(String message, String key){
+        
+        String cipher = "";
+        
+        return cipher;
+    }
+    
+    public String decode(String cipher, String key){
+        String message = "";
+        
+        return message;
+    }
+    
+    public static void main(String[] args){
+        
+    }
+}
