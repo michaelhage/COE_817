7f7c46c43f6f76037d6f2552279bfac3c0317bd5
Michael Hage <mhage222@gmail.com>
2020-02-07 00:51:47 -0500

    DES Complete, RSA Next Task

--- Labs/lab2/Project1/src/pro1/JEncrypDES.java
+++ Labs/lab2/Project1/src/pro1/JEncrypDES.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * DES Encryption Method
  */
 package pro1;
 
@@ -18,33 +16,42 @@ public class JEncrypDES {
     private static Cipher encryptCipher, decryptCipher;
     
     public static void main(String[] args){
-        String key, message, ciphertext = "";
+        String tempMessage;
         Scanner scan = new Scanner(System.in);
+        byte[] message, messageEncrypt, messageDecrypt;
         
         try{
-            encryptCipher = Cipher.getInstance("DES/CBC/PKCS5Padding");
+            KeyGenerator keyGen = KeyGenerator.getInstance("DES"); //Create DES Key Generator
+            SecretKey secKey = keyGen.generateKey(); //Generate Secret Key
+            Cipher cipher = Cipher.getInstance("DES/ECB/PKCS5Padding"); //Set Encryption Mode
             
-            decryptCipher = Cipher.getInstance("DES/CBC/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, secKey); // Cipher is able to encrypt now
             
-        }catch(NoSuchAlgorithmException | NoSuchPaddingException e){
+            System.out.println("Input message: "); // Input Message
+            tempMessage = scan.nextLine();
+            
+            message = tempMessage.getBytes(); //Convert to Bytes Array
+            
+            System.out.println("Text in Bytes: "+ message);
+            System.out.println("Text: "+ new String(message));
+            
+            messageEncrypt = cipher.doFinal(message); // Encrypt Message
+            
+            System.out.println("Text Encrypted in Bytes: "+ messageEncrypt); // Display Encrypted Message
+            System.out.println("Text Encrypted: "+ new String(messageEncrypt));
+            
+            cipher.init(Cipher.DECRYPT_MODE, secKey); // Set Cipher to Decrypt Mode
+            
+            messageDecrypt = cipher.doFinal(messageEncrypt); // Decode Message
+            
+            System.out.println("Text Decrypted in Bytes: "+ messageDecrypt); // Print Decoded Message
+            System.out.println("Text Decrypted: "+ new String(messageDecrypt));
+            
+            System.out.println("Is the Original and Decrypted Strings the same: "
+                    + new String(messageDecrypt).equals(new String(message))); // Check for Equivalence
+        }catch(Exception e){
             e.printStackTrace();
         }
-        
-        System.out.println("Input message: ");
-        message = scan.nextLine();
-        
-        System.out.println("Input Key: ");
-        key = scan.nextLine();
-        
-        //Generate encrypted key
-        
-        //Encode message
-        System.out.println("Cipher Message:\n");
-        
-        //decode cipher text
-        System.out.println("Original Message:\n"+ ciphertext);
-        System.out.println("Check For Equivalence:"+ (ciphertext.equals(message)));
-        
     }
     
 }
